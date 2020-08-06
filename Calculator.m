6b4f06527e09fe3533c7cda8c7cd4fc0e2f975f0
soerensofke <soerensofke@gmail.com>
2020-08-06 19:08:46 +0200

    Method chaining implemented using value classes

---
+++ Calculator.m
@@ -0,0 +1,38 @@
+% Soeren Sofke, IBS
+% Refereence: https://billbailey.io/2019/05/18/method-chaining-in-matlab/
+
+classdef Calculator    
+    properties
+        Data
+    end
+    
+    methods
+        function obj = Calculator(input_value)
+            obj.Data = input_value;
+        end
+        
+        function obj = add(obj, value)
+            obj.Data = obj.Data + value;
+        end
+        
+        function obj = subtract(obj, value)
+            obj.Data = obj.Data - value;
+        end
+        
+        function obj = divide(obj, value)
+            obj.Data = obj.Data / value;
+        end
+        
+        function obj = multiply(obj, value)
+            obj.Data = obj.Data * value;
+        end
+        
+        function obj = exponent(obj, value)
+            obj.Data = obj.Data^value;
+        end
+        
+        function value = get(obj)
+            value = obj.Data;
+        end
+    end
+end
---
+++ runCalculator.m
@@ -0,0 +1,2 @@
+calc = Calculator(0);
+calc.add(19).subtract(12).divide(7).multiply(3).exponent(2).get()
